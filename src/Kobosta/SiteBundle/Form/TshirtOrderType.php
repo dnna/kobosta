98370c87d78d6eacd51cbb500bfbf065f5771284
Dimosthenis Nikoudis <dnna@dnna.gr>
2012-09-26 20:27:20 +0300

    26/9/2012 changes EXCEPT sizes choice

--- src/Kobosta/SiteBundle/Form/TshirtOrderType.php
+++ src/Kobosta/SiteBundle/Form/TshirtOrderType.php
@@ -14,9 +14,9 @@ class TshirtOrderType extends AbstractType
             ->add('size', null, array('label' => 'SIZE', 'required' => true))
             ->add('name', null, array('label' => 'NAME', 'required' => true))
             ->add('surname', null, array('label' => 'SURNAME', 'required' => true))
+            ->add('phone', null, array('label' => 'TELEPHONE', 'required' => true))
             ->add('email', 'email', array('label' => 'EMAIL', 'required' => true))
             ->add('address', null, array('label' => 'ADDRESS', 'required' => true))
-            ->add('phone', null, array('label' => 'PHONE', 'required' => true))
         ;
     }
 
